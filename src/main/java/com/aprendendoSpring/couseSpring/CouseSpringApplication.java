453a19ca13c8d7c1f25eea454517fd2324d6ff96
Otavio Marques <marquesotavio251204@gmail.com>
2023-03-30 14:28:03 -0300

    subindo o projeto

---
+++ src/main/java/com/aprendendoSpring/couseSpring/CouseSpringApplication.java
@@ -0,0 +1,13 @@
+package com.aprendendoSpring.couseSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouseSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouseSpringApplication.class, args);
+	}
+
+}
